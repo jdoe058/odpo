139c4d30311b3a1290993a4f39f71f54bf25657b
Eugene Kamvol <eugene@example.com>
2026-09-17 15:50:29 +0300

    chore(timetable): удалить неиспользуемую страницу занятий

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 
 from . import views
-from .view_lesson import lessons_list_view
 from .exports.views import export_view
 
 app_name = "timetable"
 
 urlpatterns = [
     path("", views.schedule_grid_view, name="schedule_grid"),
-    path("lessons/", lessons_list_view, name="lessons_list"),
     path(
         "cycle/<int:cycle_id>/export/<slug:kind>/",
         export_view,
